11073e2c7b46989cfef3949555e79ab1c3a734a9
Arnaud MBARGA <sambambargaar153@gmail.com>
2026-04-03 11:38:50 +0200

    Correciton du bug Erreur 404 sur Render

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="Mémoires Actuariat API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"], # Le "*" autorise ton futur site Vercel à appeler l'API
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
